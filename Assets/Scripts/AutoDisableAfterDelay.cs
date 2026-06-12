ad396473d15f2c0025a0c2ba418778cc5c5583b0
make100games <make100games@gmail.com>
2026-06-12 14:14:26 +0200

    Show make100games logo at beginning

---
+++ Assets/Scripts/AutoDisableAfterDelay.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AutoDisableAfterDelay : MonoBehaviour
+{
+    [Tooltip("Duration after which this object will disable itself")]
+    private float disableAfterSeconds = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Invoke(nameof(DisableObject), disableAfterSeconds);
+    }
+
+    private void DisableObject() {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
