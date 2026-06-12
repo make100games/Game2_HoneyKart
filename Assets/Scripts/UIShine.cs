62b9e5be0174a243694e3f418b2704d0f05b03f3
make100games <make100games@gmail.com>
2026-06-12 16:37:00 +0200

    Make that shine effect even cooler

--- Assets/Scripts/UIShine.cs
+++ Assets/Scripts/UIShine.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class UIShine : MonoBehaviour
 {
     public Material mat;
-    public float speed = 1f;
+    public float duration = 20f;
+    public float waitTime = 3f;
 
-    void Update()
+    void Start()
     {
-        float pos = Mathf.PingPong(Time.time * speed, 1f);
-        mat.SetFloat("_ShinePosition", pos);
+        StartCoroutine(ShineLoop());
+    }
+
+    private IEnumerator ShineLoop() {
+        while(true) {
+            // Move shine left to right
+            float t = 0f;
+
+            while(t < duration) {
+                t += Time.deltaTime;
+                float pos = Mathf.Lerp(-0.2f, 1.2f, t / duration);
+                mat.SetFloat("_ShinePosition", pos);
+                yield return null;
+            }
+
+            // Pause before next shine effect
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 }
